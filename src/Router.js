d55eba539cd85b497f9ad3b38aa5bf93a9c2f983
JisunHan <hanccino@gmail.com>
2023-08-21 18:17:26 +0900

    ADD : 각 jsx 파일 import from 쌍따옴표에서 따옴표로 변경

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LogIn from "./pages/LogIn/LogIn";
-import Booking from "./pages/Booking/Booking";
-import BookingDone from "./pages/BookingDone/BookingDone";
-import MovieDetail from "./pages/MovieDetail/MovieDetail";
-import MovieList from "./pages/MovieList/MovieList";
-import SignUp from "./pages/SignUp/SignUp";
-import MyTicket from "./pages/MyTicket/MyTicket";
-import Payments from "./pages/Payments/Payments";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LogIn from './pages/LogIn/LogIn';
+import Booking from './pages/Booking/Booking';
+import BookingDone from './pages/BookingDone/BookingDone';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MovieList from './pages/MovieList/MovieList';
+import SignUp from './pages/SignUp/SignUp';
+import MyTicket from './pages/MyTicket/MyTicket';
+import Payments from './pages/Payments/Payments';
 
 const Router = () => {
   return (
