bd7942944f6880477114636b5fbaed6ea34c6711
jini <kjhee0394@gmail.com>
2023-08-22 20:08:25 +0900

    Modify : 로그인 UI 수정

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './LogIn.scss';
 
 const LogIn = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="contents">
       <form className="logInWrap">
@@ -10,7 +14,9 @@ const LogIn = () => {
         <input type="password" placeholder="비밀번호를 입력해주세요." />
         <button className="logIn">로그인</button>
         <p>아직 회원이 아니신가요?</p>
-        <button className="goToSignUp">엘보우시네마 회원가입 하기</button>
+        <button className="goToSignUp">
+          <Link to="/sign-up">엘보우시네마 회원가입 하기</Link>
+        </button>
       </form>
     </div>
   );
