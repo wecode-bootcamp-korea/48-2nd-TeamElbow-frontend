53ba284ade621e81d3e9798deb0775da436c136a
JisunHan <hanccino@gmail.com>
2023-08-23 14:50:06 +0900

    오류 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.scss';
 
-const Header = props => {
+const Header = () => {
   const [isLogin, setIsLogin] = useState(false);
 
   const navigate = useNavigate();
