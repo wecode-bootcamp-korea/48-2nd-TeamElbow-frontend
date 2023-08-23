a0abfc2ec65328dbd14d3c1d6cc11bb6c25baa52
JisunHan <hanccino@gmail.com>
2023-08-23 14:08:45 +0900

    Router.js common.scss reset.scss 파일 수정

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,22 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LogIn from "./pages/LogIn/LogIn";
-import Booking from "./pages/Booking/Booking";
-import BookingDone from "./pages/BookingDone/BookingDone";
-import MovieDetail from "./pages/MovieDetail/MovieDetail";
-import MovieList from "./pages/MovieList/MovieList";
-import SignUp from "./pages/SignUp/SignUp";
-import MyTicket from "./pages/MyTicket/MyTicket";
-import Payments from "./pages/Payments/Payments";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import LogIn from './pages/LogIn/LogIn';
+import Booking from './pages/Booking/Booking';
+import BookingDone from './pages/BookingDone/BookingDone';
+import MovieDetail from './pages/MovieDetail/MovieDetail';
+import MovieList from './pages/MovieList/MovieList';
+import SignUp from './pages/SignUp/SignUp';
+import MyTicket from './pages/MyTicket/MyTicket';
+import Payments from './pages/Payments/Payments';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<MovieList />} />
         <Route path="/login" element={<LogIn />} />
@@ -22,6 +27,7 @@ const Router = () => {
         <Route path="/my-ticket" element={<MyTicket />} />
         <Route path="/payments" element={<Payments />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
