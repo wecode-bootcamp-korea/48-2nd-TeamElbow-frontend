5073510dc9b7c920bab0047ba4e941e599a26c7d
JisunHan <hanccino@gmail.com>
2023-08-23 23:11:24 +0900

    sorting button onclick event 적용

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieList.scss';
 import List from '../../components/List/List';
+import movieData from '../../components/List/movieData.json';
 
 const MovieList = () => {
+  const [activeSort, setActiveSort] = useState(null);
+  const handleSortClick = sortType => {
+    setActiveSort(sortType);
+  };
+
   return (
     <div className="movieList">
       <div className="contents">
         <h2 className="title">무비차트</h2>
         <div className="sortTab">
-          <button type="button" class="btnSort on" sort-type="rate">
+          <button
+            type="button"
+            className={activeSort === 'rate' ? 'btnSort on' : 'btnSort'}
+            sort-type="rate"
+            onClick={() => handleSortClick('rate')}
+          >
             예매율순
           </button>
-          <button type="button" class="btnSort" sort-type="title">
+          <button
+            type="button"
+            className={activeSort === 'title' ? 'btnSort on' : 'btnSort'}
+            sort-type="title"
+            onClick={() => handleSortClick('title')}
+          >
             가나다순
           </button>
         </div>
