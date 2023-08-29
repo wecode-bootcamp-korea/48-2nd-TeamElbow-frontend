2d0d36ff71b6bfc19e2708173dd643736f8f5fd0
jini <kjhee0394@gmail.com>
2023-08-29 16:52:42 +0900

    Modify: Booking 페이지 기능 구현 중

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -15,7 +15,7 @@ const Booking = () => {
     setSearchParams(searchParams);
   };
   useEffect(() => {
-    fetch('/data/movieList.json', {})
+    fetch('/booking/detail', {})
       .then(res => res.json())
       .then(data => {
         setMovies(data);
@@ -79,8 +79,35 @@ const Booking = () => {
   //다시 선택하기
   const handleReset = () => {
     setDateList([]);
+    setSelectedMovie('');
     setSearchParams('');
   };
+
+  //sorting
+  const handleSort = () => {
+    searchParams.set('sort', 'rate');
+    setSearchParams(searchParams);
+    return [...movies];
+  };
+  const handleAlphabet = () => {
+    searchParams.set('sort', 'alphabet');
+    setSearchParams(searchParams);
+  };
+
+  //선택영화
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    if (movieId) {
+      fetch('/data/movieList.json', {})
+        .then(res => res.json())
+        .then(data => {
+          const selectedMovieInfo = data.find(
+            movie => movie.id === parseInt(movieId),
+          );
+          setSelectedMovie(selectedMovieInfo);
+        });
+    }
+  }, [movieId]);
 
   return (
     <div className="booking">
@@ -95,11 +122,13 @@ const Booking = () => {
           <div className="movieList movieListStyle">
             <p className="listName">영화</p>
             <div className="sortCategory">
-              <p className="select">예매순</p>
-              <p>가나다순</p>
+              <button className="select" onClick={handleSort}>
+                예매순
+              </button>
+              <button onClick={handleAlphabet}>가나다순</button>
             </div>
             <ul>
-              {movies.map(({ id, movieName, ageall }) => {
+              {movies.map(({ id, movieTitle, ageall }) => {
                 return (
                   <li key={id}>
                     <button
@@ -107,7 +136,7 @@ const Booking = () => {
                       onClick={() => handleSelect(id)}
                     >
                       <span>{ageall}</span>
-                      {movieName}
+                      {movieTitle}
                     </button>
                   </li>
                 );
@@ -116,22 +145,20 @@ const Booking = () => {
           </div>
           <div className="dateList">
             <p className="listName">날짜</p>
-            {dateList && dateList.length > 0 && (
-              <ul>
-                {dateList.map(({ id, date }) => {
-                  return (
-                    <li key={id}>
-                      <button
-                        className={dateId == id ? 'selected' : 'listBtn'}
-                        onClick={() => handleSelectDate(id)}
-                      >
-                        {date}
-                      </button>
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
+            <ul>
+              {dateList.map(({ id, date }) => {
+                return (
+                  <li key={id}>
+                    <button
+                      className={dateId == id ? 'selected' : 'listBtn'}
+                      onClick={() => handleSelectDate(id)}
+                    >
+                      {date}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
           <div className="timeList movieListStyle">
             <p className="listName">시간</p>
@@ -158,11 +185,13 @@ const Booking = () => {
           <div className="selectedMovie movieListStyle">
             <p className="listName">선택내역</p>
             <div className="infoSelectMoive">
-              <div>
-                <div className="poster"></div>
-                <p className="movieTitle">영화제목</p>
-                <p className="movieInfo">상영등급, 날짜, 상영시간</p>
-              </div>
+              {selectedMovie && (
+                <div className="info">
+                  <div className="poster"></div>
+                  <p className="movieTitle">{selectedMovie.movieTitle}</p>
+                  <p className="movieInfo">{selectedMovie.ageall}</p>
+                </div>
+              )}
               <button className="goToSeatSelect" onClick={handleInputSelect}>
                 좌석선택하기
               </button>
