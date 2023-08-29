2724648e6b2feb1d9021a42774574919c617fcec
jini <kjhee0394@gmail.com>
2023-08-29 20:18:59 +0900

    Modify : booking 페이지 기능 구현중

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -12,10 +12,13 @@ const Booking = () => {
   const movieId = searchParams.get('movieId');
   const handleSelect = id => {
     searchParams.set('movieId', id);
+    searchParams.delete('timeId');
+    searchParams.delete('dateId');
     setSearchParams(searchParams);
   };
+
   useEffect(() => {
-    fetch('/booking/detail', {})
+    fetch('/data/movieList.json', {})
       .then(res => res.json())
       .then(data => {
         setMovies(data);
@@ -26,6 +29,7 @@ const Booking = () => {
   const [dateList, setDateList] = useState([]);
   const handleSelectDate = id => {
     searchParams.set('dateId', id);
+    searchParams.delete('timeId');
     setSearchParams(searchParams);
   };
 
@@ -48,32 +52,22 @@ const Booking = () => {
   };
 
   //좌석선택하기로 이동
-  const handleInputSelect = e => {
+  const goToSelectSeats = e => {
     e.preventDefault();
+    // 실제로 최종적으로 결제 시 백엔드에 보내줄 데이터는 searchParams로 저장, 페이지 이동 시에도 searchParams 달아서 이동
+    // 각 페이지별로 보여줘야 하는 데이터(e.g. thumbnail, movieTitle)는 location 객체로 전달
 
-    const selectedAll = {
-      selectedMovie: movieId,
-      selectedDate: dateId,
-      selectedTime: timeId,
-    };
+    // 백엔드에 전달할 필요는 없지만, 각 페이지에 보여야 하기 때문에 쭉 전달해야 할 데이터
+    // const movieInfo = {
+    //   movieTitle,
+    //   moviePosterImageUrl,
+    // screeningId,
+    //movieMinimumWatchingAge,
+    //movieRunningTimeMinute
+    // };
 
-    fetch('API주소', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: selectedAll,
-    })
-      .then(res => {
-        console.log(res);
-        return res.json();
-      })
-      .then(result => {
-        console.log(result);
-        if (result !== null) {
-          navigate('/');
-        }
-      });
+    navigate(`/select-seats?${searchParams}`);
+    // navigate(`/select-seats?${searchParams}`, { state: movieInfo });
   };
 
   //다시 선택하기
@@ -84,15 +78,28 @@ const Booking = () => {
   };
 
   //sorting
-  const handleSort = () => {
-    searchParams.set('sort', 'rate');
-    setSearchParams(searchParams);
-    return [...movies];
+  const [activeSort, setActiveSort] = useState('bookingRate');
+  const bookingRate = '';
+  const alphabet = '';
+  const [uri, setUri] = useState(bookingRate);
+  // const API = 'API주소';
+
+  const handleSort = sortType => {
+    setActiveSort(sortType);
+    if (sortType === 'bookingRate') {
+      setUri(bookingRate);
+    } else if (sortType === 'alphabet') {
+      setUri(alphabet);
+    }
   };
-  const handleAlphabet = () => {
-    searchParams.set('sort', 'alphabet');
-    setSearchParams(searchParams);
-  };
+
+  useEffect(() => {
+    fetch(uri)
+      .then(response => response.json())
+      .then(result => {
+        setMovies(result);
+      });
+  }, [uri]);
 
   //선택영화
   const [selectedMovie, setSelectedMovie] = useState(null);
@@ -119,23 +126,33 @@ const Booking = () => {
           </button>
         </div>
         <div className="selectBox">
-          <div className="movieList movieListStyle">
+          <div className="selectMovieList movieListStyle">
             <p className="listName">영화</p>
             <div className="sortCategory">
-              <button className="select" onClick={handleSort}>
+              <button
+                className={activeSort === 'bookingRate' ? 'select' : null}
+                sort-type="bookingRate"
+                onClick={() => handleSort('bookingRate')}
+              >
                 예매순
               </button>
-              <button onClick={handleAlphabet}>가나다순</button>
+              <button
+                className={activeSort === 'alphabet' ? 'select' : null}
+                sort-type="alphabet"
+                onClick={() => handleSort('alphabet')}
+              >
+                가나다순
+              </button>
             </div>
             <ul>
-              {movies.map(({ id, movieTitle, ageall }) => {
+              {movies.map(({ id, movieTitle, movieMinimumWatchingAge }) => {
                 return (
                   <li key={id}>
                     <button
                       className={movieId == id ? 'selected' : 'listBtn'}
                       onClick={() => handleSelect(id)}
                     >
-                      <span>{ageall}</span>
+                      <span>{movieMinimumWatchingAge}</span>
                       {movieTitle}
                     </button>
                   </li>
@@ -165,19 +182,28 @@ const Booking = () => {
             <ul>
               {dateList
                 .find(dateData => dateData.id == dateId)
-                ?.timeList.map(({ id, time, seat }) => {
+                ?.theater.map(({ id, name, timeList }) => {
                   return (
-                    <li key={id}>
-                      <button
-                        className={
-                          timeId == id ? 'selectedTimeListBtn' : 'timeListBtn'
-                        }
-                        onClick={() => handleSelectTime(id)}
-                      >
-                        <p>{time}</p>
-                        <p>{seat}석</p>
-                      </button>
-                    </li>
+                    <div key={id}>
+                      <p>{name}</p>
+                      {timeList.map(({ id, time, seat }) => {
+                        return (
+                          <li key={id}>
+                            <button
+                              className={
+                                timeId == id
+                                  ? 'selectedTimeListBtn'
+                                  : 'timeListBtn'
+                              }
+                              onClick={() => handleSelectTime(id)}
+                            >
+                              <p>{time}</p>
+                              <p>{seat}석</p>
+                            </button>
+                          </li>
+                        );
+                      })}
+                    </div>
                   );
                 })}
             </ul>
@@ -187,12 +213,20 @@ const Booking = () => {
             <div className="infoSelectMoive">
               {selectedMovie && (
                 <div className="info">
-                  <div className="poster"></div>
+                  <div className="poster">
+                    <img src={selectedMovie.moviePosterImageUrl} />
+                  </div>
                   <p className="movieTitle">{selectedMovie.movieTitle}</p>
-                  <p className="movieInfo">{selectedMovie.ageall}</p>
+                  <p className="movieInfo">
+                    {selectedMovie.movieMinimumWatchingAge} 관람가
+                  </p>
+                  <p className="movieInfo">
+                    상영시간
+                    {selectedMovie.movieRunningTimeMinute}분
+                  </p>
                 </div>
               )}
-              <button className="goToSeatSelect" onClick={handleInputSelect}>
+              <button className="goToSeatSelect" onClick={goToSelectSeats}>
                 좌석선택하기
               </button>
             </div>
