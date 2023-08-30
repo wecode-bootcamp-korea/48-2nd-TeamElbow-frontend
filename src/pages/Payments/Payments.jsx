d0c81dbc2ca501bd0dc53dc20a7def0d543dae4b
jini <kjhee0394@gmail.com>
2023-08-30 13:31:41 +0900

    Add : 결제페이지

--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Payments.scss';
 
 const Payments = () => {
-  return <div></div>;
+  const [usePoint, setUsePoint] = useState('');
+
+  return (
+    <div className="payments contents">
+      <h1>결제</h1>
+      <div className="usePoint">
+        <p>포인트결제</p>
+        <div>
+          <span>사용 가능한 포인트</span>
+          <input type="text"></input>
+        </div>
+        <div>
+          <span>사용할 포인트</span>
+          <input type="text" name="toUsePoint"></input>
+        </div>
+      </div>
+      <div className="useCard">
+        <p>카드결제</p>
+      </div>
+    </div>
+  );
 };
 
 export default Payments;
